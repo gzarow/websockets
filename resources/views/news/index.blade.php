25fda217c2bdb59843f353f50c25fe9b8e57c8f3
Grzegorz Żarów <g.zarow@ideo.pl>
2023-08-27 18:10:14 +0200

    js code refactoring zmienne w string html

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -27,21 +27,22 @@
         const channels = @json($channels);
         channels.forEach(channel => {
             Echo.channel(channel)
-                .listen('NewsAdded', (data) => { console.log(data.news);
+                .listen('NewsAdded', (data) => {
                     $('#websockets').prepend(
-                    `<div class="py-12">
-                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                                <img src="icons/`+channel+`.png" alt="`+channel+`" class="p-2" style="width:60px;" />
-                                <div  class="px-6  whitespace-nowrap text-black-600">
-                                    `+ data.news.title +
-                                `</div>
-                                <div class="p-6 text-gray-900 dark:text-gray-100">`+
-                                    data.news.content +
-                                `</div>
+                        `<div class="py-12">
+                            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                                    <img src="icons/${channel}.png" alt="${channel}.png" class="p-2" style="width:60px;" />
+                                    <div  class="px-6  whitespace-nowrap text-black-600">
+                                        ${data.news.title}
+                                    </div>
+                                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                                        ${data.news.content}
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>`);
+                        </div>`
+                    );
                 });
              });    
         });
