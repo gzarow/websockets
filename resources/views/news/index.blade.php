ab233ebf46987ab7ff84e6e4ef6583bf1782a642
Grzegorz Żarów <g.zarow@ideo.pl>
2023-09-17 10:11:09 +0200

    widok wiadomości oraz ikonki

---
+++ resources/views/news/index.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Twoje Wiadomości') }}
+        </h2>
+    </x-slot>
+    <div id="websockets">
+    </div>
+    @foreach ($news as $item)
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <img src="icons/{{$item->channel->slug}}.png" alt="{{$item->channel->slug}}" class="p-2" style="width:60px;" />
+                    <div  class="px-6  whitespace-nowrap text-black-600">
+                        {{ $item->title }}
+                    </div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ $item->content }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</x-app-layout>
